7fb71b5308e39710c372201944bf6824ae0950cf
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-07-26 16:52:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,6 +36,24 @@ this.setState({filter: e.currentTarget.value})
       contacts: prevState.contacts.filter(contact => contact.id !== id)
     }))
   }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+        this.setState({contacts: parsedContacts })
+    }
+  
+}
+
+  componentDidUpdate(prevProps, prevState,) {
+  
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+}
+
    render() {
     const { filter} = this.state;
     const normalisFilter = this.state.filter.toLowerCase();
